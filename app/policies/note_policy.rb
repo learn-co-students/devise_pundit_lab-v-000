468eaaa6f579d5053beb104ce37f9af0101412cc
Lukeghenco <lukeghenco@gmail.com>
2016-02-10 10:25:42 -0500

    Done.

--- app/policies/note_policy.rb
+++ app/policies/note_policy.rb
@@ -1,7 +1,13 @@
 class NotePolicy < ApplicationPolicy
+  attr_reader :note
+
+  def initialize(user, note)
+    super(user, note)
+    @note = record
+  end
 
   def update
     user.admin? or not record.published?
   end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119074046) do
+ActiveRecord::Schema.define(version: 20160210143021) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "content"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160119074046) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
