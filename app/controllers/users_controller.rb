dfb314b4a6a30745287e8c58aa120c5a59cd02a3
Adam King <adam.king0126@gmail.com>
2016-03-09 15:04:36 -0600

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    if params[:id] == current_user.id
+      @user = current_user
+    elsif current_user.role == "vip" || current_user.role == "admin"
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+      flash[:alert] = "Access denied."
+    end
+  end
+
+
+end
