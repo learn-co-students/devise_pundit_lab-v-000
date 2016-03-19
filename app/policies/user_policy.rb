1608df37c6d6cd38c77a869cd76a07247c7ba837
coltonstaab1 <colton.staab@gmail.com>
2016-03-19 16:18:44 -0700

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,25 @@
 class UserPolicy < ApplicationPolicy
 
-  def show?
-    user.admin? || record == user
+  attr_reader :current_user, :user
+
+  def initialize(current_user, user)
+      @current_user = current_user
+      @user = user
   end
 
+  def index?
+    @current_user.admin?
+  end
+
+  def show?
+    @current_user == @user || @current_user.admin?
+  end
+
+  def update?
+    @current_user.admin?
+  end
+
+  def destroy?
+    @current_user.admin?
+  end
 end
