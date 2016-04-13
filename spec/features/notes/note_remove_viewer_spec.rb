c50c5da1014bd4c6b6958063413b00543dc48d95
Dakota Martinez <dakotaleemusic@gmail.com>
2016-04-13 01:28:48 +0000

    users can add and remove viewers from their notes

--- spec/features/notes/note_remove_viewer_spec.rb
+++ spec/features/notes/note_remove_viewer_spec.rb
@@ -1,13 +1,13 @@
 include Warden::Test::Helpers
 Warden.test_mode!
 
-feature 'Users can add viewers to their notes', :devise do
+feature 'Users can remove viewers from their notes', :devise do
 
   after(:each) do
     Warden.test_reset!
   end
     
-  scenario 'user can add viewers to notes they created' do
+  scenario 'user can remove viewers from notes they created' do
     user = FactoryGirl.create(:user)
     other_user = FactoryGirl.create(:user, email: "otheremail@me.com", name: "otheruser")
     login_as(user, :scope => :user)
@@ -16,10 +16,14 @@ feature 'Users can add viewers to their notes', :devise do
     fill_in "note_content", with: "my new note - edited"
     fill_in "note_visible_to", with: "otheruser"
     click_button "save"
+    visit edit_note_path(new_note)
+    fill_in "note_content", with: "otheruser can't see it anymore"
+    fill_in "note_visible_to", with: ""
+    click_button "save"
     click_link "Sign out"
     login_as(other_user, :scope => :user)
     visit notes_path
-    expect(page).to have_content('my new note - edited')
+    expect(page).not_to have_content("otheruser can't see it anymore")
   end
 
 end
