8e356eef56c3721a93d86f26b7b832d6400f6ef6
shinyrachel <rachel.kory@gmail.com>
2016-04-14 14:56:21 -0400

    policies specs passing

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,25 @@
-class UserPolicy < ApplicationPolicy
+class UserPolicy
+  attr_reader :current_user, :model
+
+  def initialize(current_user, model)
+    @current_user = current_user
+    @model = model
+  end
+
+  def index?
+    current_user.admin?
+  end
+
+  def show?
+    current_user.admin? || current_user == model
+  end
+
+  def update?
+    current_user.admin?
+  end
+
+  def destroy?
+    current_user.admin?
+  end
+
 end
