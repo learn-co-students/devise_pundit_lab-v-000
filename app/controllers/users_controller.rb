6073976e32e4363d2cc6e9ea33052102088f8c8f
Brad1974 <danieltbrad@gmail.com>
2016-04-23 16:25:26 -0700

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+  # def create
+  # end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+end
