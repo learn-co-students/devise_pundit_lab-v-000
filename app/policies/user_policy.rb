9fc0f8e21b25e052ce880e634eced1ff172388af
cernanb <cernanb@gmail.com>
2016-04-25 23:50:22 -0700

    created pundit authorizations

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,19 @@
 class UserPolicy < ApplicationPolicy
+  attr_reader :user
+
+  def show?
+    record == user || user.admin?
+  end
+
+  def index?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin? && (record != user)
+  end
 end
