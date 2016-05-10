a9898dd73df921a01894a03dd00267dec267581a
seanliu93 <sean93liu@gmail.com>
2016-05-10 01:09:19 -0400

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,21 @@
 class UserPolicy < ApplicationPolicy
+  def index?
+     user.admin?
+   end
+ 
+   def destroy?
+     user.admin? && record != user
+   end
+ 
+   def update?
+     user.admin?
+   end
+ 
+   def show?
+     user.admin? || record == user
+     #usually we are used to asking "record.user" (i.e., post.user > who is a post's user?)
+     #but here we are trying to limit a user's show page
+       #instead we ask is the @user (the pundit record) the current_user (the pundit user)
+   end
+ 
 end
