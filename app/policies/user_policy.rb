d979269b43b25a1bb7cb4d453ca89a4b783e81b6
James Novak <jamesnovak90@gmail.com>
2016-05-13 21:16:03 -0400

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,24 @@
 class UserPolicy < ApplicationPolicy
+  attr_reader :user, :note
+
+  def initialize(user, note)
+    @user = user
+    @note = note
+  end
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || self.user == note
+  end
+
+  def update?
+    user.admin? || note.try(:user) == user
+  end
+
+  def destroy?
+    user.admin?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  #get 'visitors/index'
+
+  get 'pages/about', to: 'visitors#about', as: :about
+
+  get '/users', to: 'users#index', as: :users
+
   devise_for :users
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'notes#index'
+  root 'visitors#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
