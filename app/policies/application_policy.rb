c0ec92e99d8ca0c0c8aaf25cbb4ea0019051752b
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-05-24 16:44:29 -0700

    Done.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,2 +1,19 @@
 class ApplicationPolicy
+  attr_reader :user, :record
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+  def index?
+    user.admin?
+  end
+  def show?
+    user == record || user.admin?
+  end
+  def update?
+    user == record || user.admin?
+  end
+  def destroy?
+    user.admin? && (record != user)
+  end
 end
