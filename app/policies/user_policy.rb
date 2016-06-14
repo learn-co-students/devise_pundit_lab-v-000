ccc980033f359143eba033f1ea628eca9e1bc144
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-14 22:19:20 +0000

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,19 @@
 class UserPolicy < ApplicationPolicy
+  
+  def index?
+    user.admin?
+  end
+  
+  def show?
+    user.admin? || user.id == record.id
+  end
+  
+  def update?
+    user.admin?
+  end
+  
+  def destroy?
+    user.admin?
+  end
+  
 end
