b4a7e814ac6dfff2b7f45ff435fb4a26e90e8dec
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-07-07 21:22:31 +0000

    finished user policy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sign_in_sign_out_links
+    if user_signed_in?
+      link_to 'Sign out', destroy_user_session_path, method: :delete
+    else
+      link_to 'Sign in', new_user_session_path
+    end
+  end
 end
