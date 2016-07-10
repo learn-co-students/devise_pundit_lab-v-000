70d7c4d5a20a2b5fca310c4b5993d70e8e44f7e9
depippo <jdepippo@gmail.com>
2016-07-10 19:53:53 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require 'yaml'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -19,7 +19,7 @@ module DevisePunditLab
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
-
+config.web_console.development_only = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
