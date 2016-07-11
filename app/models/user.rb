fadd242f445c960eef34af8d668ce979aee3a0b5
Savannah Scott <sascott624@gmail.com>
2016-07-11 18:08:12 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+         
   has_many :notes
   has_many :viewers
   has_many :readable, through: :viewers, source: :note
+
+  enum role: [:user, :vip, :admin]
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
