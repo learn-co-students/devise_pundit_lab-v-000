516ec52fe133c38ea5a356e8ac9180a3d2b929d1
wedesignstudios <deelden@gmail.com>
2016-08-12 14:22:54 -0500

    work in progress

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,19 @@
 class UserPolicy < ApplicationPolicy
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || user == record
+  end
+
+  def update?
+    user.admin? || user == record
+  end
+
+  def destroy?
+    user.admin?
+  end
+
 end
