269431a0675451ad7ba114be5a295776d875edac
Jason <jasonkwong11@gmail.com>
2016-08-13 13:41:20 -0400

    done

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,19 @@
 class UserPolicy < ApplicationPolicy
+  
+  def index?
+    user.admin?
+  end
+
+  def show?
+    record == user || user.admin?
+  end
+
+  def update?
+    user.admin? || record == user
+  end
+
+  def destroy?
+    user.admin?
+  end
+
 end
