23b88d5b621d2b786c8ddfc5356ae22a6d7f8470
arturusfury <bri.reynolds@gmail.com>
2016-08-18 13:53:15 -0500

    Done.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,4 @@
 class NotesController < ApplicationController
-  
-  def new
-    
-  end
-  
   def create
     note = Note.new(note_params)
     note.user = current_user
@@ -13,21 +8,19 @@ class NotesController < ApplicationController
 
   def update
     @note.update(note_params)
-    redirect_to '/'    
+    redirect_to '/'
   end
-  
+
   def edit
     @note = Note.find(params[:id])
   end
-  
+
   def show
   end
 
   def index
     @notes = Note.none
-    if current_user
-      @notes = current_user.readable
-    end
+    @notes = current_user.readable if current_user
   end
 
   private
