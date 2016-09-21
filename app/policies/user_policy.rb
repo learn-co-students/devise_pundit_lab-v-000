2a3673f0bb1db4dd45865b15ee9e2127067adfc0
tpstar <hanlee12@gmail.com>
2016-09-21 00:16:47 -0400

    stuck at factorygirl role

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,15 @@
+require 'pry'
+
 class UserPolicy < ApplicationPolicy
+  attr_reader :user, :role
+  def initialize(user, role)
+    @user = user
+    @role = role
+  binding.pry
+  end
+
+  def index?
+    user.admin?
+  end
+
 end
