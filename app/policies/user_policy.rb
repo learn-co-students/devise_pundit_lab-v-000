69e00b286313e179dc837957a600610e1c3fc4e2
tpstar <hanlee12@gmail.com>
2016-09-22 00:13:30 -0400

    finish policy spec

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,15 +1,26 @@
 require 'pry'
 
 class UserPolicy < ApplicationPolicy
-  # attr_reader :user
-  # def initialize(user, other_user)
-  #   @user = user
-  #   @other_user = other_user
-  # binding.pry
-  # end
-  #
-  # def index?
-  #   user.admin?
-  # end
+  attr_reader :user, :the_user
+  def initialize(user, the_user)
+    @user = user
+    @the_user = the_user
+  end
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+    the_user == user || user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
 
 end
