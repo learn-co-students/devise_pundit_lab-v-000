fd31e2f910159b8aa424d4e8e56441b7d2a8f491
auranbuckles <auran59@gmail.com>
2016-10-07 00:01:36 -0400

    Done.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,2 +1,14 @@
 class ApplicationPolicy
+
+	attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def show?
+    scope.where(:id => record.id).exists?
+  end
+
 end
