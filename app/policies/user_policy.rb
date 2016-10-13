4a0a65f7aa23f524a753ed2291b6d69aa63c968f
Kathleen <kayjsmyth@gmail.com>
2016-10-13 10:03:15 -0400

    thanks Iangit add .

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,23 @@
 class UserPolicy < ApplicationPolicy
+	attr_reader :current_user, :other_user
+	def initialize(current_user, other_user)
+		@current_user = current_user
+		@other_user = other_user
+	end
+
+	def index?
+		current_user.admin?
+	end
+
+	def show?
+		current_user.admin? || current_user.try(:id) == current_user
+	end
+
+	def update?
+		current_user.admin?
+	end
+
+	def destroy?
+		current_user.admin?
+	end
 end
