31a818635bfd4e5440baff597293522b94fcc28e
tylertaylor <vikingtyty@gmail.com>
2016-10-14 13:13:49 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    binding.pry
+    if user_signed_in? ## Learn expert instructed me to add a check here
+      # to set @current_user because for some reason current_user was broken in views
+      @current_user = current_user
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'static/about'
+
   devise_for :users
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +10,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'notes#index'
+  get '/about', to: 'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
