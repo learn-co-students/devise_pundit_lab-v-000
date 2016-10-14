d5ce1940020e01fdb04ebb2145a11f281270c762
tylertaylor <vikingtyty@gmail.com>
2016-10-14 14:18:21 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # In users#show we call 'authorize @user' which throws this not authorized error
+  # instead we'll use this method to show a flash error
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(error)
+    flash[:error] = "Access denied."
+    redirect_to root_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    authorize @user
   end
 
 end
