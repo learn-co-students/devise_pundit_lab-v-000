e208eee4f28472e7358860d439362a791da678b6
Lenn Hypolite <lenntriniman@hotmail.com>
2016-10-18 23:25:26 +0000

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,20 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    unless authorize @user
-      redirect_to users_path, alert: "Access denied"
+      @user = User.find(params[:id])
+      unless authorize @user
+        redirect_to users_path, alert: "Access denied."
+      end
     end
-  end
 
   def update
   end
 
   def destroy
-
+    @user = User.find(params[:id])
+      unless authorize @user
+        redirect_to users_path, alert: "Access denied."
+      end
   end
 
 private
