98199d6836283d06dc699dc981fd7250cd7c77ca
Traci7822 <Tracir@gmail.com>
2016-10-19 20:11:05 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    authorize @user
-    redirect_to users_path(@user)
+    if authorize @user
+      redirect_to users_path(@user)
+    else
+      redirect_to users_path, :alert => "Access denied."
+    end
   end
 
   def update
