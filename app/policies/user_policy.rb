4ce764e3dc9b96e697af4205893d7c58913bdd6a
Yechiel Kalmenson <chilikkal@gmail.com>
2016-11-04 04:46:18 +0000

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,16 @@
 class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+
+    def index?
+      user.admin?
+    end
+
+    def show?
+      record == user
+    end
+
+    def update?
+      user.admin? || user.moderator? || record.try(:user) == user
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'notes#index'
+
+  resources :users, only: [:index, :show, :update, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
