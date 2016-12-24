50826089ea6af9a21eaaa236d5e067a99af4fcde
Ivan Pilot <ivanpilot@gmail.com>
2016-12-24 04:06:30 +0100

    all tests passed except 1 pending

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,51 @@
 class UserPolicy < ApplicationPolicy
+
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || user == record
+  end
+
+  def update?
+    user.admin? || user == record
+  end
+
+  def destroy?
+    user.admin? && user != record
+  end
+
+  def permitted_attributes
+    if user.admin? || user == record
+      [:email]
+    end
+  end
+
+
+  class Scope
+
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(id: user.id)
+      end
+    end
+  end
+
 end
