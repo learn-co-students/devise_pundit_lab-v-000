3a3fa58beb063aacddb023bee04cd3b7958b8524
Laurent <lmilne360@gmail.com>
2017-01-05 16:13:37 -0500

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,45 @@
 class UserPolicy < ApplicationPolicy
+	attr_reader :user, :record
+
+	def initialize(user, record)
+		@user = user
+		@record = record
+	end
+
+	def show?
+		user == record || user.admin?
+	end
+
+	def index?
+		user.admin?
+	end
+
+	def update?
+		user.admin?
+	end
+
+	def destroy?
+		user.admin? 
+	end
+
+
+
+	 class Scope
+
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(id: user.id)
+      end
+    end
+  end
+
 end
