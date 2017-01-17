2fcfb22ed65aa9301a2158e5cec5255abe92a445
mcclains2003 <mcclains2003@gmail.com>
2017-01-17 16:15:46 -0600

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,17 @@
 class UserPolicy < ApplicationPolicy
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+    record == user || user.admin?
+  end
+
+  def update?
+  end
+
+  def destroy?
+  end
+
 end
