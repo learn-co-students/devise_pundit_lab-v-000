907ed32c8d0bc4fb3ebf734ba02fc6faa2c938d2
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-02-12 23:22:23 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
+  def index
+    @user = current_user
+  end
 
+  def show
+    @user = User.find_by_id(params[:id])
+    authorize @user
+  end
+
+  def edit
+    @user = User.find_by_id(params[:id])
+    authorize @user
+  end
 end
