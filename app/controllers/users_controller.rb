057bd15b4bbce6760eb8640ebd316ccb0dac9e97
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-02-16 10:06:37 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,11 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
+  def show
+    @user = User.find(params[:id])
+    render plain: "Access denied." unless @user == current_user
+  end
+
+end
