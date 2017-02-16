e6ed7aea2bfbba677d0ab2a396d43c80b735c23a
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-02-16 12:05:47 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    authorize @users
   end
 
   def show
-    @user = User.find(params[:id])
-    render plain: "Access denied." unless @user == current_user
+    @user = policy_scope(User)
+    #User.find(params[:id])
+    render plain: "Access denied." unless @user
+    #@user = authorize User.find(params[:id])
+
   end
 
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,19 @@
 class UserPolicy < ApplicationPolicy
+
+  def index?
+    @user.admin?
+  end
+
+
+  class Scope < Scope
+
+
+    def resolve
+      if @user.admin?
+        scope.all
+      else
+        scope.where(id: user.id).exists?
+      end
+    end
+  end
 end
