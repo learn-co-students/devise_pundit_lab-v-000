20da729864daa51c33e5f247f751accaefc5beb9
tddycks <tadzio.smith@gmail.com>
2017-02-21 16:02:19 -0500

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,25 @@
 class UserPolicy < ApplicationPolicy
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || record == user
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  def permitted_attributes
+    if user.admin? || record == user
+         [:name, :email, :password]
+    end
+  end
+
 end
