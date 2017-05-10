b32c2da4639e662e1d4dc4bf1db741102519e6f0
Michael <michael.ries@gmail.com>
2017-05-10 14:19:45 -0500

    pass existing feature specs for views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include Pundit
   protect_from_forgery with: :exception
+
+  def goodbye
+  end
+
+  private
+  def after_sign_out_path_for(resource_or_scope)
+    '/goodbye'
+  end
 end
