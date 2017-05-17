b595f07caacef7810be32b2b0da4eb70c23b3980
Howard DeVennish <howarddevennish@gmail.com>
2017-05-17 21:13:32 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
 
   enum role: [:normal, :moderator, :admin]
 
