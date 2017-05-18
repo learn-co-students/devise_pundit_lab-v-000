ed412676ff313e0e77b7e64e8518b367dbe2f715
github username <github email address>
2017-05-17 23:52:01 -0500

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,19 @@
 class UserPolicy < ApplicationPolicy
+	attr_reader :user
+
+	def index?
+		user.admin?
+	end
+
+	def show?
+		@record == @user || user.admin?
+	end
+
+	def update?
+		user.admin?
+	end
+
+	def destroy?
+		user.admin?
+	end
 end
