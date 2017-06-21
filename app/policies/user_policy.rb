64a1baf769742c50da8a1604e15cbee2e7d30303
Joseph Kim <josephkim0224@gmail.com>
2017-06-21 00:34:04 +0000

    Done.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,42 @@
 class UserPolicy < ApplicationPolicy
+  attr_reader :user, :user2
+
+  def initialize(user, user2)
+    @user = user
+    @user2 = user2
+  end
+
+  def index?
+    user.admin?
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    user == user2 || user.admin?
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    user == user2 || user.admin?
+  end
+
+  def destroy?
+    user.admin? && user != user2
+  end
+
+  class Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(id: user2.id)
+      end
+    end
+  end
 end
