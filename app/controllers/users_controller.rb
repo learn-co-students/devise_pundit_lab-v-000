f96fe09888cd6fd63b47b9301f4fda6269ab6064
Andrew <ajoeford@gmail.com>
2017-06-27 19:51:17 -0700

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @user = current_user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    authorize @user
+  end
+end
